4d3f43e0ae71ef6af95f1658b76d0c0a9acc09b3
Andrew Noble <aenoble@gmail.com>
2014-12-30 17:11:52 +0000

    Adding Scorebar snapshots.

--- scoreboard_vcr.sh
+++ scoreboard_vcr.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-datepath=`date +%Y-%m-%d`
 leaguestat_key=f109cf290fcf50d4
 leaguestat_clientcode=lhjmq
 format=json
@@ -10,12 +9,14 @@ view=scorebar
 
 output_path="JSON/${view}_vcr"
 
+datepath=`date +%Y-%m-%d`
 url="http://chlcluster.leaguestat.com/feed/index.php?feed=sitekit&key=${leaguestat_key}&client_code=${leaguestat_clientcode}&season_id=178&view=${view}&date=${datepath}&fmt=${format}"
 
 while true
 do
+    current_datepath=`date +%Y-%m-%d`
     timepath=`date +%H:%M:%S`
-    curl $url > ${output_path}/${datepath}-${timepath}.json
+    curl $url > ${output_path}/${current_datepath}-${timepath}.json
     # wait 5 minutes
     sleep $(expr 60 \* 5)
 done
